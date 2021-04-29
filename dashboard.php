df372b8ec93a14b96ba635082a6e7153b6ab895a
Karma91430 <arthur.delerue91@gmail.com>
2021-04-29 23:57:35 +0200

    yu

--- dashboard.php
+++ dashboard.php
@@ -55,42 +55,8 @@
 
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<script src="javascripts/Graph/graph.js"></script>
 <script>
-  new Chart(document.getElementById("line-chart"), {
-  type: 'line',
-  data: {
-    labels: [1,2,3,4,5,6,7,8],
-    datasets: [{
-        data: [2,4,3,6,5,8,7,9],
-        borderColor: "#FF589E",
-        fill: true,
-        backgroundColor	:"rgba(255, 88, 158,0.1)",
-        pointBackgroundColor: "#FF589E"
-      }
-    ]
-  },
-
-
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    legend: {
-    display: false,
-    },
-    scales: {
-        xAxes: [{
-            gridLines: {
-                display:false
-            }
-        }],
-        yAxes: [{
-            gridLines: {
-                display:false
-            }
-        }]
-    }
-
-  }
-});
+  graph1();
 </script>
 </html>
