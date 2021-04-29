0cf966a4d2c0c40543c8277f99d8caa3293b2afb
Karma91430 <arthur.delerue91@gmail.com>
2021-04-29 20:58:47 +0200

    trt

--- connexion.php
+++ connexion.php
@@ -32,6 +32,7 @@
 
         <div class="forgot_container">
           <a href="#">Mot de passe oublié</a>
+          <a href="inscription.html">Nous n'avez pas encore de compte ? Inscrivez-vous</a>
         </div>
       </div>
 
