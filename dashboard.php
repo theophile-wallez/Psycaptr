4427097adc5b33a1e0604707eb72d85cfa474c11
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-04-29 16:30:14 +0200

    kljdaz

--- dashboard.php
+++ dashboard.php
@@ -20,7 +20,7 @@
       session_start();
 
       echo "<p>";
-      echo "Salut ".$_SESSION["Prenom"]."\n".$_SESSION["Nom"];
+      echo "Salut jlkdja ".$_SESSION["Prenom"]."\n".$_SESSION["Nom"];
       echo "</p>";
      ?>
   </div>
