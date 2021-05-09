fe89802dc37fa0dae97dc1fed4d5971fd2eefec0
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-09 09:53:00 +0200

    Update navBar.php

--- Ressources/Pages/navBar.php
+++ Ressources/Pages/navBar.php
@@ -3,6 +3,7 @@ session_start();
 
 function displayNavBar(){
     //Mettre conditions
+    
     echo '<a class="link-logo" href="index.php#">';
     echo '<img class="header-logo" src="Ressources/Images/logo.png" alt="Logo Psycaptr" draggable="false">';
     echo '</a>';
@@ -12,7 +13,12 @@ function displayNavBar(){
     echo '<a class="header-link FAQ" href="index.php#FAQ-link" draggable="false">FAQ</a>';
     echo '<a class="header-link Contact" href="index.php#contact_link" draggable="false">Contact</a>';
     echo '</div>';
-    echo '<a class="header-connect-container" href="Ressources/Pages/connexion.php#" draggable="false">Connexion</a>';
+    if($_SESSION['login'] = 1)) {
+        echo '<a class="header-connect-container" href="Ressources/Pages/dashboard.php#" draggable="false">Dashboard</a>';
+	}
+    else{
+        echo '<a class="header-connect-container" href="Ressources/Pages/connexion.php#" draggable="false">Connexion</a>';
+    }
 }
 
 
