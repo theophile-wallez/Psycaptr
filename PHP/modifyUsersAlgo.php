52b2a74821b5f523e58f375b585a27779ef037bf
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-18 22:14:38 +0200

    Update modifyUsersAlgo.php

--- PHP/modifyUsersAlgo.php
+++ PHP/modifyUsersAlgo.php
@@ -7,7 +7,7 @@
     if(isset($_POST['search'])) { 
         $search = convertInput($_POST['search']);
         echo '<p>il y a quelque chose</p>';
-        $sql = "SELECT * FROM Utilisateurs where Nom like $search%";
+        $sql = "SELECT * FROM Utilisateurs where Nom like '$search%'";
         // order by Nom asc
         // header('Location:../Ressources/Pages/modifyUsers.php'); //pas sur de ça
         header("Refresh:0; url=../Ressources/Pages/modifyUsers.php");
