7df6c1fab9a5df47a728d382dedfe30e731b5405
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-18 22:06:49 +0200

    Update modifyUsersAlgo.php

--- PHP/modifyUsersAlgo.php
+++ PHP/modifyUsersAlgo.php
@@ -9,6 +9,9 @@
         echo '<p>il y a quelque chose</p>';
         $sql = 'SELECT * FROM Utilisateurs where Nom like $search% order by Nom asc';
         // header('Location:../Ressources/Pages/modifyUsers.php'); //pas sur de ça
+        header("Refresh:0; url=../Ressources/Pages/modifyUsers.php");
+
+        // unset($_POST['search']);
     }
     else {
         echo '<p>il y a rien dans la barre de recherche</p>';
