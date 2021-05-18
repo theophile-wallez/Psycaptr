51c363ed50b19220116fdde55d2bab2a03a6b942
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-18 22:41:20 +0200

    Update modifyUsersAlgo.php

--- PHP/modifyUsersAlgo.php
+++ PHP/modifyUsersAlgo.php
@@ -18,7 +18,6 @@
     if(!$result = $bdd -> query($sql)){
       echo "Échec de la requête SQL : (" . $bdd->errno . ") " . $bdd->error;
     }
-    echo 'Les résultats sont : '.$result;
 
     // Recuperation des resultats
     while($row = $result -> fetch_row()){
