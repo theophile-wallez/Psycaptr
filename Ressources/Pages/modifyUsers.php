4f4a4cd257faa4ad2b155f96d920cc6ced17a59b
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-18 18:53:32 +0200

    changement d'emplacement de l'algo

--- Ressources/Pages/modifyUsers.php
+++ Ressources/Pages/modifyUsers.php
@@ -48,32 +48,8 @@
       <div class="id-container">Identifiant</div>
       <div class="date-container">Date d'inscription</div>
     </div>
-    <?php 
-    
-    require_once('../../PHP/connectDatabase.php');
 
-    $sql = 'SELECT * FROM Utilisateurs order by Nom asc';
-
-    if(!$result = $bdd -> query($sql)){
-      echo "Échec de la requête SQL : (" . $bdd->errno . ") " . $bdd->error;
-    }
-    // Recuperation des resultats
-    while($row = $result -> fetch_row()){
-        $Id=$row[0];
-        $Mail=$row[1];
-        $Date_Inscription = date("d-m-Y",strtotime($row[3]));
-        $Nom = $row[4];
-        $Prenom = $row[5];
-
-        echo '<div class="line-container user-container">';
-        echo '<div class="nom-container">'.$Nom.'</div>';
-        echo '<div class="prenom-container">'.$Prenom.'</div>';
-        echo '<div class="mail-container">'.$Mail.'</div>';
-        echo '<div class="id-container">'.$Id.'</div>';
-        echo '<div class="date-container">'.$Date_Inscription.'</div>';
-        echo '</div>';
-    }
-    ?>
+    <?php require_once('../../PHP/modifyUsersAlgo.php');?>
 
 
     <!-- <div class="user-container">
