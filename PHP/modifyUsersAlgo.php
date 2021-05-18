6854c57810db2cda09920ce4e7b49e20f11bb96b
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-18 19:48:52 +0200

    Update modifyUsersAlgo.php

--- PHP/modifyUsersAlgo.php
+++ PHP/modifyUsersAlgo.php
@@ -8,7 +8,7 @@
     if(isset($search)) { 
         
         echo '<p>il y a quelque chose</p>';
-        $sql = 'SELECT * FROM Utilisateurs where Nom like '$search%' order by Nom asc';
+        $sql = 'SELECT * FROM Utilisateurs where Nom like $search% order by Nom asc';
         header('Location:../Ressources/Pages/modifyUsers.php'); //pas sur de ça
     }
     else {
