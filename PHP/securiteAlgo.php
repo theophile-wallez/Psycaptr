971dc253b68d77bfc7cff7c274b206785b15cc49
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-24 16:11:55 +0200

    Update securiteAlgo.php

--- PHP/securiteAlgo.php
+++ PHP/securiteAlgo.php
@@ -1,12 +1,12 @@
 <!-- Ou est-ce que ce php est appelé ? -->
 <?php 
-	session_start();
-    if($_SESSION['login'] != 1 | !isset($_SESSION['login'])) {
-      if(!isset($_SESSION['lastActivity']) && (time()-$_SESSION['lastActivity'])>1800){
-        unset($_SESSION);
-        header('Location:../index.php#');
-        exit();
-      }
-    }
-    $_SESSION['lastActivity']= time();
+	// session_start();
+    // if($_SESSION['login'] != 1 | !isset($_SESSION['login'])) {
+    //   if(!isset($_SESSION['lastActivity']) && (time()-$_SESSION['lastActivity'])>1800){
+    //     unset($_SESSION);
+    //     header('Location:../index.php#');
+    //     exit();
+    //   }
+    // }
+    // $_SESSION['lastActivity']= time();
 ?>
