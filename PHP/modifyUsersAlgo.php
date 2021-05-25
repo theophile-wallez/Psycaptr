91c6b37069c1a7ea59b1828ef14c0f7880e9cd02
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-25 03:46:43 +0200

    Update modifyUsersAlgo.php

--- PHP/modifyUsersAlgo.php
+++ PHP/modifyUsersAlgo.php
@@ -63,7 +63,7 @@
         $Prenom = $row[5];
 
         //On génère une ligne qui correpond à chaque utilisateurs
-        echo '<form class="line-container user-container" action="modifyUsers.php" method="POST">';
+        echo '<form class="line-container user-container" action="../../PHP/modifyUsersAlgo.php" method="POST">';
         echo    '<input type="text" name="Nom" value="'.$Nom.'" required>';
         echo    '<input type="text" name="Prenom" value="'.$Prenom.'" required>';
         echo    '<input input type="email" name="Mail" value="'.$Mail.'" required>';
