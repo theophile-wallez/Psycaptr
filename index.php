9b892d1a603f635776feb6433f1018277bc2b402
MathildeG93 <madoumath7@gmail.com>
2021-05-29 17:38:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -281,7 +281,7 @@
 
   <footer id="footer" class="centered" >
 	<div class="CGU"> 
-		<a href="Ressources/Pages/CGU.php"> CGU </a>
+		<a href="Ressources/Pages/CGU.html"> CGU </a>
 	</div>
   </footer>
 
