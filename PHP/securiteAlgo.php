6784c39c81d9dec89eabe35c713d622e11562bca
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-29 21:53:36 +0200

    Update securiteAlgo.php

--- PHP/securiteAlgo.php
+++ PHP/securiteAlgo.php
@@ -3,7 +3,7 @@
     if($_SESSION['login'] != 1 | !isset($_SESSION['login'])) {
       if(!isset($_SESSION['lastActivity']) && (time()-$_SESSION['lastActivity'])>1800){
         $_SESSION = array(); 
-		header('Location:/');
+		    header('Location:/');
         exit();
       }
     }
