58ea6ab5453255799c071b51c05caff1bc7858a9
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-29 04:34:42 +0200

    Update modifyUsersAlgo.php

--- PHP/modifyUsersAlgo.php
+++ PHP/modifyUsersAlgo.php
@@ -150,7 +150,7 @@
             $sql = 'SELECT * FROM Utilisateurs order by Nom asc';
         }
         else if($_SESSION['userType']=='medecin'){
-            echo "Id du médecin : ".$IdMedecin;
+            echo $IdMedecin;
             $sql = "SELECT * FROM Patient where Id_Medecin = '$IdMedecin' order by Nom asc";
         }
     }
