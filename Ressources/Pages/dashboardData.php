7406d4d52c29a9a77c6d3e4a8396c84370e2818f
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-31 01:29:00 +0200

    Update dashboardData.php

--- Ressources/Pages/dashboardData.php
+++ Ressources/Pages/dashboardData.php
@@ -7,10 +7,10 @@
     $MailPatient   = $_SESSION['MailPatient'];
 
     if (in_array($PrenomPatient{0}, ['a','e','i','o','u'])) { 
-      $de = 'd';
+      $de = 'd ';
      }
      else {
-       $de = 'de';
+       $de = 'de ';
      }
 ?>
 
@@ -29,11 +29,11 @@
 <body>
 
   <div class="dashboard_container">
-    <h1 class="dashboard_title">Voici les données <?php echo $de.' '.$PrenomPatient?></h1>
+    <h1 class="dashboard_title">Voici les données <?php echo $de.$PrenomPatient?></h1>
     <div class="main_part_container">
         <div class="part_1_container ">
           <div class="graph graph-1">
-            <h4 class="canvas_title">Évolution du score <?php echo $de.' '.$PrenomPatient?></h4>
+            <h4 class="canvas_title">Évolution du score <?php echo $de.$PrenomPatient?></h4>
             <div class="canvas_container">
              <canvas id="line-chart"></canvas>
             </div>
