beb0a17db54203ccb6c65327e211b0d2098b0f5a
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-05-31 02:55:41 +0200

    Update connexionAlgo.php

--- PHP/connexionAlgo.php
+++ PHP/connexionAlgo.php
@@ -58,8 +58,9 @@
 
 				//Récupère la dernière adresse IP utilisée pour se connecter
 				if($IP != $row[6]){
+					$sql = "UPDATE Utilisateurs SET IP='$Mail' WHERE Id='$row[0]'";
 					$result -> free_result();
-					$sql = "UPDATE Utilisateurs SET IP='$Mail' WHERE Id='$_SESSION['IdMedecin']'";
+
 					if(!$bdd -> query($sql)){
 						echo "Échec lors de la création du compte : (" . $bdd->errno . ") " . $bdd->error;
 						echo " |".$Id;
