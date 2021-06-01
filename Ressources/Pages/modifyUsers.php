1f6b650a5e06da8bf6ceac8d9ae8b84b7440c134
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-06-01 14:49:35 +0200

    Update modifyUsers.php

--- Ressources/Pages/modifyUsers.php
+++ Ressources/Pages/modifyUsers.php
@@ -94,7 +94,7 @@ if($_SESSION['userType']=='admin'){
     echo '<div class="prenom-container">Prénom</div>';
     echo '<div class="mail-container">Adresse mail</div>';
     echo '<div class="id-container">Identifiant</div>';
-    echo '<div class="date-container">Date d',"'inscription</div>";
+    echo '<div class="date-container">Date d\'inscription</div>';
     echo '</div>';
     echo '</div>';
     echo '</section>';
