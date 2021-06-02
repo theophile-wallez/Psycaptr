9e847fb09c05dedcdda6e6e2efe2dbfeb87824a8
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-06-02 11:25:05 +0200

    Update modifyProfile.php

--- PHP/modifyProfile.php
+++ PHP/modifyProfile.php
@@ -56,6 +56,7 @@ if(isset($_POST['modifyProfile'])){
       exit();
     }
   }
+  
 
   /*----------------------------------------------------------------*/
   //Ne rentre pas dans le if pour les admins ??
