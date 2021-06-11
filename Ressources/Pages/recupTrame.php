2f98f4fc7991d35fff096908fa8eebe2410e5888
MathildeG93 <madoumath7@gmail.com>
2021-06-11 10:04:40 +0200

    Update recupTrame.php

--- Ressources/Pages/recupTrame.php
+++ Ressources/Pages/recupTrame.php
@@ -1,5 +1,6 @@
 <?php
 
+	//récup données
 	$ch = curl_init();
 	curl_setopt(
 		$ch, 
@@ -11,5 +12,24 @@
 	curl_close($ch);
 	echo "Raw Data:<br />";
 	echo("$data");
+	
+	//Affichage 1 ligne = 1 trame d'1 capteur
+	$data_tab = str_split($data,33);
+	echo "Tabular Data:<br />";
+	for($i=0, $size=count($data_tab);$i<$size;$i++){
+		echo "Trame $i: $data_tab[$i]<br />";
+	}
 
+	//Décoder 1 trame
+	$trame = $data_tab[1];
+	// décodage avec des substring
+	$t = substr($trame,0,1);
+	$o = substr($trame,1,4);
+	// ...
+	
+	// décodage avec sscanf
+	list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) = 
+		sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+	echo("<br/>$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br/>");
+	
 ?>
