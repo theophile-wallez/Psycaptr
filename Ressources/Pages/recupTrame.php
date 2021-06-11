3de6efb75cb53da3bf9b82945f2196087a97c3da
Hns Hlr <74055973+Flumxz@users.noreply.github.com>
2021-06-11 19:26:08 +0200

    e

--- Ressources/Pages/recupTrame.php
+++ Ressources/Pages/recupTrame.php
@@ -15,22 +15,19 @@ echo("$data<br /><br />");
 $data = substr($data, 91);
 $data_tab = str_split($data,33);
 
+// Dernière trame reçue :
+$trame = $data_tab[-1];
+
+list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
+    sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
+
+// Listing de toutes les trames
 echo "Tabular Data:<br /><br />";
 for($i=0, $size=count($data_tab); $i<$size; $i++){
     echo "Trame $i: $data_tab[$i]<br />";
     echo "TypeTrame: ",substr($data_tab[$i],0,1)," | NumObjet: ",substr($data_tab[$i],1,4)," | TypeRequete: ",substr($data_tab[$i],5,1)," | TypeCapteur: ",substr($data_tab[$i],6,1)," | NumCapteur: ",substr($data_tab[$i],7,2)," | ValeurLue: ",substr($data_tab[$i],9,4)," | NumTrame: ",substr($data_tab[$i],13,4)," | Checksum: ",substr($data_tab[$i],17,2)," | Annee: ",substr($data_tab[$i],19,4)," | Mois: ",substr($data_tab[$i],23,2)," | Jour: ",substr($data_tab[$i],25,2)," | Heure: ",substr($data_tab[$i],27,2)," | Minutes: ",substr($data_tab[$i],29,2)," | Secondes: ",substr($data_tab[$i],31,2);
     echo "<br /><br />";
 }
-
-
-$trame = $data_tab[1];
-// décodage avec des substring
-$t = substr($trame,0,1);
-$o = substr($trame,1,4);
-// …
-// décodage avec sscanf
-list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
-    sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
 
 ?>
