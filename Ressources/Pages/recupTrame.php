29f2d896e879a1ea424ead01ab383fb0f55a8f08
Hns Hlr <74055973+Flumxz@users.noreply.github.com>
2021-06-11 19:18:49 +0200

    look

--- Ressources/Pages/recupTrame.php
+++ Ressources/Pages/recupTrame.php
@@ -10,11 +10,11 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $data = curl_exec($ch);
 curl_close($ch);
 echo "Raw Data:<br />";
-echo("$data");
+echo("$data<br />");
 
 $data_tab = substr($data, 91);
 $data_tab = str_split($data_tab,33);
-echo "Tabular Data:<br />";
+echo "Tabular Data:<br /><br />";
 for($i=0, $size=count($data_tab); $i<$size; $i++){
     echo "Trame $i:<br />";
     echo "TypeTrame: ",substr($data_tab[$i],0,1)," | NumObjet: ",substr($data_tab[$i],1,4)," | TypeRequete: ",substr($data_tab[$i],5,1)," | TypeCapteur: ",substr($data_tab[$i],6,1)," | NumCapteur: ",substr($data_tab[$i],7,2)," | ValeurLue: ",substr($data_tab[$i],9,4)," | NumTrame: ",substr($data_tab[$i],13,4)," | Checksum: ",substr($data_tab[$i],17,2)," | Annee: ",substr($data_tab[$i],19,4)," | Mois: ",substr($data_tab[$i],23,2)," | Jour: ",substr($data_tab[$i],25,2)," | Heure: ",substr($data_tab[$i],27,2)," | Minutes: ",substr($data_tab[$i],29,2)," | Secondes: ",substr($data_tab[$i],31,2);
