e1c2e63abbd829f56e58ac2dd0fbbf4c2e29f546
MathildeG93 <madoumath7@gmail.com>
2021-06-11 09:59:33 +0200

    Update recupTrame.php

--- Ressources/Pages/recupTrame.php
+++ Ressources/Pages/recupTrame.php
@@ -1,7 +1,10 @@
 <?php
 
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL,"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
+	curl_setopt(
+		$ch, 
+		CURLOPT_URL,
+		"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
 	curl_setopt($ch, CURLOPT_HEADER, FALSE);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	$data = curl_exec($ch);
