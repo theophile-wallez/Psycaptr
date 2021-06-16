12bf04879dec7875ab23770ee5a59ccde27cb2d5
Hns Hlr <74055973+Flumxz@users.noreply.github.com>
2021-06-16 10:54:10 +0200

    Update recupTrame.php

--- PHP/recupTrame.php
+++ PHP/recupTrame.php
@@ -27,13 +27,13 @@ list($typeTrame, $numObjet, $typeRequest, $typeCapteur, $numCapteur, $valeurLue,
     sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
 echo("$typeTrame,$numObjet,$typeRequest,$typeCapteur,$numCapteur,$valeurLue,$numTrame,$checkSum,$year,$month,$day,$hour,$min,$sec<br /><br />");
 
-// Listing de toutes les trames
-echo "Tabular Data: La Database de l'ISEP contient ",count($data_tab)-1," trames.<br /><br />";
-for($i=0, $size=count($data_tab); $i<$size-1; $i++){
-    echo "Trame $i: $data_tab[$i]<br />";
-    echo "TypeTrame: ",substr($data_tab[$i],0,1)," | NumObjet: ",substr($data_tab[$i],1,4)," | TypeRequete: ",substr($data_tab[$i],5,1)," | TypeCapteur: ",substr($data_tab[$i],6,1)," | NumCapteur: ",substr($data_tab[$i],7,2)," | ValeurLue: ",substr($data_tab[$i],9,4)," | NumTrame: ",substr($data_tab[$i],13,4)," | Checksum: ",substr($data_tab[$i],17,2)," | Annee: ",substr($data_tab[$i],19,4)," | Mois: ",substr($data_tab[$i],23,2)," | Jour: ",substr($data_tab[$i],25,2)," | Heure: ",substr($data_tab[$i],27,2)," | Minutes: ",substr($data_tab[$i],29,2)," | Secondes: ",substr($data_tab[$i],31,2);
-    echo "<br /><br />";
-}
+// // Listing de toutes les trames
+// echo "Tabular Data: La Database de l'ISEP contient ",count($data_tab)-1," trames.<br /><br />";
+// for($i=0, $size=count($data_tab); $i<$size-1; $i++){
+//     echo "Trame $i: $data_tab[$i]<br />";
+//     echo "TypeTrame: ",substr($data_tab[$i],0,1)," | NumObjet: ",substr($data_tab[$i],1,4)," | TypeRequete: ",substr($data_tab[$i],5,1)," | TypeCapteur: ",substr($data_tab[$i],6,1)," | NumCapteur: ",substr($data_tab[$i],7,2)," | ValeurLue: ",substr($data_tab[$i],9,4)," | NumTrame: ",substr($data_tab[$i],13,4)," | Checksum: ",substr($data_tab[$i],17,2)," | Annee: ",substr($data_tab[$i],19,4)," | Mois: ",substr($data_tab[$i],23,2)," | Jour: ",substr($data_tab[$i],25,2)," | Heure: ",substr($data_tab[$i],27,2)," | Minutes: ",substr($data_tab[$i],29,2)," | Secondes: ",substr($data_tab[$i],31,2);
+//     echo "<br /><br />";
+// }
 
 // Connexion à la database
 require('connectDatabase.php'); //Connexion à la database
@@ -61,6 +61,8 @@ if($result = $bdd -> query($sql)) {
 foreach ($data_tab as $trameISEP) {
     $isEqual=0;
     foreach ($tramesTableau as $trameDB) {
+
+        echo "Trame site ISEP : ".$trameISEP." | Trame Database : ".$trameDB;
         if($trameISEP==$trameDB) {
             $isEqual=1;
             echo "C egal";
