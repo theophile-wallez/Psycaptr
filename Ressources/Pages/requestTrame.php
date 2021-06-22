564af369f2748a541a20702edd5d7a578e73c25f
Karma91430 <arthur.delerue91@gmail.com>
2021-06-22 09:26:50 +0200

    Update requestTrame.php

--- Ressources/Pages/requestTrame.php
+++ Ressources/Pages/requestTrame.php
@@ -37,7 +37,7 @@
       </div>
     </form>
 
-	<a href="http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G9Dy&TRAME=".<?php
+	<a href="http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G9Dy&TRAME="<?php
 
 	// Exemple de trame : 1 G9Dy 13011234 FFFF 1520210611094457
 
