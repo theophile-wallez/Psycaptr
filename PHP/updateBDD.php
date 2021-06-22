4af8d945ad2ee08d51621c25fcfe3dd7c2142d20
Hns Hlr <74055973+Flumxz@users.noreply.github.com>
2021-06-22 11:49:41 +0200

    Update updateBDD.php

--- PHP/updateBDD.php
+++ PHP/updateBDD.php
@@ -42,7 +42,9 @@ foreach ($data_tab as $trameISEP) {
     list($typeTrame, $numObjet, $typeRequest, $typeCapteur, $numCapteur, $valeurLue, $numTrame, $checkSum, $year, $month, $day, $hour, $min, $sec) =
         sscanf($trameISEP,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
 
-    $sqlAjout = "INSERT INTO `Trames` (`TypeTrame`, `NumObjet`, `TypeRequete`, `TypeCapteur`, `NumCapteur`, `ValeurLue`, `NumTrame`, `Checksum`, `Annee`, `Mois`, `Jour`, `Heure`, `Minutes`, `Secondes`) VALUES ('$typeTrame','$numObjet','$typeRequest','$typeCapteur','$numCapteur','$valeurLue','$numTrame','$checkSum','$year','$month','$day','$hour','$min','$sec')";
+    $Id=IdGenerator(11);
+
+    $sqlAjout = "INSERT INTO `Trames` (`ID`,`TypeTrame`, `NumObjet`, `TypeRequete`, `TypeCapteur`, `NumCapteur`, `ValeurLue`, `NumTrame`, `Checksum`, `Annee`, `Mois`, `Jour`, `Heure`, `Minutes`, `Secondes`) VALUES ('$Id','$typeTrame','$numObjet','$typeRequest','$typeCapteur','$numCapteur','$valeurLue','$numTrame','$checkSum','$year','$month','$day','$hour','$min','$sec')";
     if ($bdd -> query($sqlAjout)) {
         echo "Ajout OK";
     } 
