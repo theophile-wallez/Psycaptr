331fdbccba79df24d2e9b268f3230006e653e68a
Hns Hlr <74055973+Flumxz@users.noreply.github.com>
2021-06-22 10:09:07 +0200

    Update requestTrame.php

--- Ressources/Pages/requestTrame.php
+++ Ressources/Pages/requestTrame.php
@@ -12,47 +12,61 @@
   <link rel="stylesheet" href="../Style/title.css"/>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  <link rel="icon" href="../Images/Logo_light.png" type="image/icon type">
-  <title>Gestion de la FAQ • Psycaptr</title>
 </head>
 
 
 
 <body>
-  <h1>Gestionnaire de la FAQ</h1>
-  <section class="content-container">
-    <h2>Ajout d'une question à la FAQ</h2>
+	<a href="http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G9Dy&TRAME= <?php $trame = "1G9Dy23013585FFFF15"; echo $trame; ?>"> Test d'envoi d'une trame Site-Objet</a>
 
-   <form class="container-form" action="../../PHP/modifyFAQAlgo.php" method="POST">
-      <div class="count_container count_container_grand">
-        <h3 class="count count_plus">+</h3>
-      </div>
-      <div class="inputs_container">
-        <div class="line-container user-container">
-          <input class="question-container" type="text" placeholder="Contenu de la question" name="Question" required/>
-          <div class="valider_changement remove"><button type="submit" name="addFAQ"><i class="fa fa-plus"></i></button></div>
-        </div>
-        <textarea class="reponse-container" type="text" name="Reponse" placeholder="Contenu de la réponse " required></textarea>
-      </div>
-    </form>
+  <label for="pet-select">Choose a pet:</label>
 
-	<a href="http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G9Dy&TRAME=<?php
+  <form action="http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=G9Dy&TRAME= <?php echo $trame; ?>" method="post">
+    <select name="Capteur">
+        <option value="" disabled selected>Choose option</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+    </select>
 
-	// Exemple de trame : 1 G9Dy 2 3 01 3585 FFFF 15
+    <input type="submit" name="submit" vlaue="Choose options">
+  </form>
 
-	$trame = "1G9Dy23013585FFFF15";
-	echo $trame;
+  <?php
 
-	?>"> Test d'envoi d'une trame Site-Objet</a>
+  $typeTrame="1";
+  $numObjet="G9Dy"; 
+  $typeRequest="2"; 
 
+  $typeCapteur=$selected;
+  $numCapteur="01";
 
-    <!-- Affichage de la liste des utilisateurs -->
-    <?php require('../../PHP/modifyFAQAlgo.php');?>
+  $valeurLue="0000";
+
+  $numTrame="FEDC";
+  $checkSum="15";
+
+  $trame=$typeTrame.$numObjet.$typeRequest.$typeCapteur.$numCapteur.$valeurLue.$numTrame.$checkSum;
+
+  if(isset($_POST['submit'])) { 
+    if(!empty($_POST['Capteur'])) {
+        $selected = $_POST['Capteur'];
+        echo 'You have chosen: ' . $selected;
+    } else {
+        echo 'Please select the value.';
+    }
+  }
+
+  ?>
 
   </section>
 </body>
 </html>
+
+
+
+
 
 <!-- Ces balises de script fixent un bug de Chrome qui déclanche les
 animations des :hover au lancement de la page -->
