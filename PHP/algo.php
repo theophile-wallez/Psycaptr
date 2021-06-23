7d871c20bdb712b26e98d7862b36befc646553a3
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-06-23 16:13:18 +0200

    daz

--- PHP/algo.php
+++ PHP/algo.php
@@ -67,8 +67,8 @@
     curl_close($ch);
     $data = substr($data, 91);
 
+
     
-    
-    return hexdec(substr($data_tab[count(str_split($data,33))-2],9,4));
+    return substr($data_tab[count(str_split($data,33))-2],9,4);
   }
 ?>
