aae13b6eb3de478ed23f954a579338bb126c7b38
theophilewall <66305625+theophilewall@users.noreply.github.com>
2021-06-23 15:56:40 +0200

    Update lunchTest.php

--- Ressources/Pages/lunchTest.php
+++ Ressources/Pages/lunchTest.php
@@ -27,7 +27,7 @@
     <h1 class="test_title">Choisissez le test que vous désirez effectuer</h1>
     <section class="content-container">
     <div class="line-container">
-        <a class="test-container" href="https://psycaptr.tk" target="theframe" onclick="window.open('attenteTest')" >
+        <a class="test-container" href="https://psycaptr.tk" target="theframe" >
           <div class="testIcon-container">
             <i class="fas fa-music"></i>
           </div>
